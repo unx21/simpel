f18bee46387946fc41c99312a1c4af2ee0c0e2e7
Reysekha <gansrey97@gmail.com>
2022-04-11 21:33:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('SEKHA\nASSIST', {
+CFonts.say('simpel bot', {
   colors: ['#00FFFF'],
   font: 'block',
   align: 'center',
